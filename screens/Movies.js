8598fc9b825097c796b2ed67617fa7e1e85de083
Lee sang Jik <sangjik9206@gmail.com>
2023-06-13 17:12:04 +0900

    INFINITE SCROLL
    MASTER CLASS RN

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,6 +1,6 @@
 import {
   useInfiniteQuery,
-  useQueries,
+  useQuery,
   useQueryClient,
 } from "@tanstack/react-query";
 import React, { useState } from "react";
@@ -8,7 +8,6 @@ import { Dimensions, FlatList } from "react-native";
 import styled from "styled-components/native";
 import Swiper from "react-native-swiper";
 import HMedia from "../components/HMedia";
-import VMedia from "../components/VMedia";
 import Slide from "../components/Slide";
 import Loader from "../components/Loader";
 import HList from "../components/HList";
@@ -31,17 +30,10 @@ const options = {
 };
 //---------------------------
 
-const ListContainer = styled.View`
-  margin-bottom: 40px;
-`;
-
 const ComingSoonTitle = styled(ListTitle)`
   margin-bottom: 20px;
 `;
 
-const VSeparator = styled.View`
-  width: 20px;
-`;
 const HSeparator = styled.View`
   width: 20px;
 `;
@@ -51,25 +43,44 @@ const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 const Movies = () => {
   const [refreshing, setRefreshing] = useState(false);
   const queryClient = useQueryClient();
-  const Items = ["now_playing", "popular"];
-  const Results = useQueries({
-    queries: Items.map((item) => {
-      return {
-        queryKey: ["Moives", item],
-        queryFn: () =>
-          fetch(
-            `https://api.themoviedb.org/3/movie/${item}?language=en-US&page=1`,
-            options
-          ).then((response) => response.json()),
-      };
-    }),
-  });
-
-  const upcomingData = useInfiniteQuery(["upcoming"], () =>
+  const { data: nowPlayingData } = useQuery(["Moives", 1], () =>
     fetch(
-      `https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1`,
+      `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1 
+    }`,
       options
     ).then((response) => response.json())
+  );
+
+  const popularData = useInfiniteQuery(
+    ["popular", 1],
+    ({ pageParam }) =>
+      fetch(
+        `https://api.themoviedb.org/3/movie/popular?language=en-US&page=${
+          pageParam ?? 1
+        }`,
+        options
+      ).then((response) => response.json()),
+    {
+      getNextPageParam: (CurrentPage) => {
+        const nextPage = CurrentPage.page + 1;
+        return nextPage > CurrentPage.total_pages ? null : nextPage;
+      },
+    }
+  );
+
+  const upcomingData = useInfiniteQuery(
+    ["upcoming", 1],
+    ({ pageParam }) =>
+      fetch(
+        `https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=${pageParam}`,
+        options
+      ).then((response) => response.json()),
+    {
+      getNextPageParam: (CurrentPage) => {
+        const nextPage = CurrentPage.page + 1;
+        return nextPage > CurrentPage.total_pages ? null : nextPage;
+      },
+    }
   );
 
   const onRefresh = async () => {
@@ -78,14 +89,15 @@ const Movies = () => {
     setRefreshing(false);
   };
 
-  const loadMore = () => {
-    alert("load More!");
+  const MoreUpcomingMovies = () => {
+    if (upcomingData.hasNextPage) {
+      upcomingData.fetchNextPage();
+    }
   };
 
-  return Results[0]?.data ? (
+  return nowPlayingData && upcomingData && popularData ? (
     <FlatList
-      onEndReached={loadMore}
-      onEndReachedThreshold={0.2}
+      onEndReached={MoreUpcomingMovies}
       onRefresh={onRefresh}
       refreshing={refreshing}
       ListHeaderComponent={
@@ -103,8 +115,8 @@ const Movies = () => {
               height: SCREEN_HEIGHT / 4,
             }}
           >
-            {Results[0]?.data
-              ? Results[0].data.results.map((movie) => (
+            {nowPlayingData?.results
+              ? nowPlayingData.results.map((movie) => (
                   <Slide
                     key={movie.id}
                     backdropPath={movie.backdrop_path || ""}
@@ -118,13 +130,18 @@ const Movies = () => {
               : null}
           </Swiper>
 
-          {Results[1]?.data ? (
-            <HList title="Popular Movies" data={Results[1].data.results} />
+          {popularData.data ? (
+            <HList
+              title="Popular Movies"
+              data={popularData?.data?.pages
+                ?.map((page) => page.results)
+                .flat()}
+            />
           ) : null}
           <ComingSoonTitle>Coming soon</ComingSoonTitle>
         </>
       }
-      data={upcomingData.data.pages.map((page) => page.results).flat()}
+      data={upcomingData?.data?.pages?.map((page) => page.results).flat()}
       keyExtractor={(item) => item.id + ""}
       ItemSeparatorComponent={HSeparator}
       renderItem={({ item }) => (
